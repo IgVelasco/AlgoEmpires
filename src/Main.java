9f1f1067ad07d482572c64c17ddefe7d0459a644
BautiRo <37890416+BautiRo@users.noreply.github.com>
2018-11-06 21:06:34 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-}
 
-public String test () {
-    
-    return "Test";
+    public String test () {
+        
+        return "Test";
+    }
 }
