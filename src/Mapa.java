ddccdf01949937b5499863f5018a3868723b08e0
Bauti <bautirodriguez@gmail.com>
2018-11-11 20:51:44 -0300

    Creacion del mapa con el tamanio.

---
+++ src/Mapa.java
@@ -0,0 +1,20 @@
+public class Mapa {
+
+    private int cantCeldas;
+    Casillero [][] mapa;
+
+    public Mapa(int x, int y) {
+        mapa = new Casillero[x][y];
+
+        cantCeldas = x*y;
+
+        for (int i = 0 ; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                Casillero casillero = new Casillero();
+                mapa[i][j] = casillero;
+            }
+        }
+    }
+
+    public int getCantCeldas() { return this.cantCeldas;}
+}
