453f40c44f9b0403cbf55a584ae2f698bf51a52e
DonatoJP <33533052+DonatoJP@users.noreply.github.com>
2018-11-12 09:02:12 -0300

    Create Casillero

--- src/espacio/Casillero.java
+++ src/espacio/Casillero.java
@@ -1,4 +1,22 @@
 package espacio;
 
+import contenibles.Contenible;
+
 public class Casillero {
+
+    private boolean estaOcupado = false;
+    private Contenible contenido;
+
+    public boolean casilleroEstaOcupado() {
+        return this.estaOcupado;
+    }
+
+    public void contener(Contenible unContenible) {
+        contenido = unContenible;
+        estaOcupado = true;
+    }
+
+    public Contenible getContenido() {
+        return this.contenido;
+    }
 }
---
+++ test/CasilleroTest.java
@@ -0,0 +1,33 @@
+import contenibles.Contenible;
+import espacio.Casillero;
+import unidades.Aldeano;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+public class CasilleroTest {
+
+    @Test
+    public void testCasilleroSeCreaSinContenido(){
+        Casillero unCasillero = new Casillero();
+        assertFalse(String.valueOf(false), unCasillero.casilleroEstaOcupado() );
+
+
+    }
+
+    @Test
+    public void testCasilleroPuedeContenerUnaUnidadContenible(){
+        Contenible unAldeano = new Aldeano();
+        Casillero unCasillero = new Casillero();
+
+        unCasillero.contener(unAldeano);
+
+        assertTrue(String.valueOf(true), unCasillero.casilleroEstaOcupado());
+        assertEquals( unAldeano, unCasillero.getContenido());
+
+
+    }
+}
