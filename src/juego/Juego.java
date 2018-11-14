037fb45b8fdf16d9d3f8cb00f794c7e79a625ed8
Bauti <bautirodriguez@gmail.com>
2018-11-14 01:46:01 -0300

    siguienteTurno y refactor Juego Class

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -11,12 +11,15 @@ public class Juego {
 
 
     public Juego(int DimensionHorizontal, int DimensionVertical) throws CasilleroOcupado, ExcedeLimiteDelMapa {
+        Turno turno = new Turno();
         jugadores = new Jugador[2];
 
         mapa = new Mapa(DimensionHorizontal, DimensionVertical);
 
         jugadores[1] = new Jugador(mapa, DimensionHorizontal / 2, 0);
         jugadores[0] = new Jugador(mapa, DimensionHorizontal / 2, DimensionVertical - 4);
+
+        turno.primerTurno();
 
 
     }
--- test/TurnoTest.java
+++ test/TurnoTest.java
@@ -7,10 +7,30 @@ public class TurnoTest {
     public void testEmpiezaJugadorEstaEnIntervalo() {
         Turno unTurno = new Turno();
 
-      int value = unTurno.primerTurno();
+        int value = unTurno.primerTurno();
 
-      assert(value <= 2 && value >= 1 );
+        assert(value == 1 || value == 0 );
 
     }
 
+
+    @Test
+    public void testTurnoCambiaCorrectamente() {
+        Turno unTurno = new Turno();
+        int turnoActual ;
+
+        int turnoInicial = unTurno.primerTurno();
+
+        assert(turnoInicial == 1 || turnoInicial == 0 );
+
+        turnoActual = unTurno.siguienteTurno();
+
+        assert (turnoInicial != turnoActual);
+
+
+        turnoActual = unTurno.siguienteTurno();
+
+        assert (turnoInicial == turnoActual);
+
+    }
 }
