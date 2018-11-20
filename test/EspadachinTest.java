ef848d9d8caf7524da78bff4b11b7cc7184b90c4
Bauti <bautirodriguez97@gmail.com>
2018-11-20 11:22:33 -0300

    realizarMovimiento se encarga del movimiento en cualquier direccion. Pruebas hechas

--- test/EspadachinTest.java
+++ test/EspadachinTest.java
@@ -16,68 +16,6 @@ public class EspadachinTest {
 
         assertEquals(100, unEspadachin.getVida());
     }
-    @Test
-    public void testEspadachinMoverHorizontalmenteLoRealizaCorrectamente() throws CasilleroOcupado, ExcedeLimiteDelMapa, UnidadYaUtilizada {
-        Mapa mapa = new Mapa(10,10);
-        Espadachin unEspadachin = new Espadachin(null);
-
-        mapa.colocarUnidadEn(unEspadachin,0,0);
-        unEspadachin.moverDerecha(mapa);
-        assertEquals(unEspadachin, mapa.getContenido(1,0));
-    }
-
-    @Test
-    public void testEspadachinMoverseHorizontalmenteActualizaPosicion() throws CasilleroOcupado, ExcedeLimiteDelMapa, UnidadYaUtilizada {
-        Mapa mapa = new Mapa(10,10);
-        Espadachin unEspadachin = new Espadachin(null);
-
-        mapa.colocarUnidadEn(unEspadachin,1,1);
-        unEspadachin.moverIzquierda(mapa);
-        assertEquals(0, unEspadachin.getPosicionHorizontal());
-    }
-
-    @Test
-    public void testEspadachinMoverEnVerticalLoRealizaCorrectamente() throws CasilleroOcupado, ExcedeLimiteDelMapa, UnidadYaUtilizada {
-        Mapa mapa = new Mapa(10,10);
-        Espadachin unEspadachin = new Espadachin(null);
-
-        mapa.colocarUnidadEn(unEspadachin,1,1);
-        unEspadachin.moverArriba(mapa);
-        assertEquals(unEspadachin, mapa.getContenido(1,2));
-    }
-
-    @Test
-    public void testEspadachinMoverVerticalActualizaPosicion() throws CasilleroOcupado, ExcedeLimiteDelMapa, UnidadYaUtilizada {
-        Mapa mapa = new Mapa(10,10);
-        Espadachin unEspadachin = new Espadachin(null);
-
-        mapa.colocarUnidadEn(unEspadachin,1,1);
-        unEspadachin.moverAbajo(mapa);
-        assertEquals(0, unEspadachin.getPosicionVertical());
-    }
-
-    @Test
-    public void testEspadachinMoverseDiagonalLoRealizaCorrectamente() throws CasilleroOcupado, ExcedeLimiteDelMapa, UnidadYaUtilizada {
-        Mapa mapa = new Mapa(10,10);
-        Espadachin unEspadachin = new Espadachin(null);
-
-        mapa.colocarUnidadEn(unEspadachin,1,1);
-        unEspadachin.moverDerechaSuperior(mapa);
-        assertEquals(unEspadachin, mapa.getContenido(2,2));
-    }
-
-
-    @Test
-    public void testEspadachinMoverseDiagonalActualizaPosicion() throws CasilleroOcupado, ExcedeLimiteDelMapa, UnidadYaUtilizada {
-        Mapa mapa = new Mapa(10,10);
-        Espadachin unEspadachin = new Espadachin(null);
-
-        mapa.colocarUnidadEn(unEspadachin,1,1);
-        unEspadachin.moverIzquierdaInferior(mapa);
-        assertEquals(0, unEspadachin.getPosicionHorizontal());
-        assertEquals(0,unEspadachin.getPosicionVertical());
-    }
-
 
     @Test(expected = ContenibleFueraDeRango.class)
     public void testEspadachinAtacaEspadachinFueraDeRango() throws ContenibleFueraDeRango, CasilleroOcupado, ExcedeLimiteDelMapa, ContenibleDelMismoJugador {
