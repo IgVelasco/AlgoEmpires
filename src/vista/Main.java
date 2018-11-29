92885f46addc59e8a93127807df6a8f33c438fd1
iPetrignani <32021777+iPetrignani@users.noreply.github.com>
2018-11-29 20:09:37 -0300

    Polish stage

--- src/vista/Main.java
+++ src/vista/Main.java
@@ -3,16 +3,18 @@ package vista;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.media.AudioClip;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
@@ -26,49 +28,55 @@ public class Main extends Application {
         Application.launch(args);
     }
 
-    public void start(Stage stage) throws Exception {
-        Image imagenTitulo = new Image(TITULO_ESCENA, true);
-        ImageView titulo = new ImageView(imagenTitulo);
-
-        Alert wip = new Alert(Alert.AlertType.INFORMATION);
-        wip.setResizable(false);
-        wip.setTitle("WIP");
-        wip.setContentText("WIP");
-
-        Button botonJugar = new Button();
-        botonJugar.setId("botonJugar");
-        botonJugar.setOnAction(actionEvent -> wip.show());
-
-        Button botonCreditos = new Button();
-        botonCreditos.setId("botonCreditos");
-        botonCreditos.setOnAction(actionEvent -> wip.show());
-
-        Button botonSalir = new Button();
-        botonSalir.setId("botonSalir");
-        botonSalir.setOnAction(actionEvent -> Platform.exit());
+    public void start(Stage escenario) {
+        AudioClip sonidoInicio = new AudioClip(
+                getClass().getResource("sonidos/sonido_inicio.mp3").toExternalForm()
+        );
+        sonidoInicio.play();
 
         BorderPane raiz = new BorderPane();
-        GridPane grilla = new GridPane();
+
+        Image imagenTitulo = new Image(TITULO_ESCENA, true);
+        ImageView titulo = new ImageView(imagenTitulo);
+        BorderPane.setAlignment(titulo, Pos.CENTER);
+        BorderPane.setMargin(titulo, new Insets(32, 0, 0, 0));
+
+        raiz.setTop(titulo);
+
+        GridPane botonera = new GridPane();
+
+        Button botonJugar = new Button("¡Jugar!");
+        botonJugar.setId("botonJugar");
+        botonJugar.setOnAction(new BotonEventHandler(escenario, botonJugar));
+
+        Button botonAcercaDe = new Button("Acerca de...");
+        botonAcercaDe.setId("botonAcercaDe");
+        botonAcercaDe.setOnAction(new BotonEventHandler(escenario, botonAcercaDe));
+
+        Button botonSalir = new Button("Salir");
+        botonSalir.setId("botonSalir");
+        botonSalir.setOnAction(actionEvent -> Platform.exit());
 
         ColumnConstraints columnas = new ColumnConstraints();
         columnas.setHalignment(HPos.CENTER);
 
-        grilla.getColumnConstraints().add(columnas);
-        grilla.setAlignment(Pos.CENTER);
-        grilla.setVgap(32);
+        botonera.getColumnConstraints().add(columnas);
+        botonera.setAlignment(Pos.CENTER);
+        botonera.setVgap(16);
 
-        grilla.add(botonJugar, 0, 0);
-        grilla.add(botonCreditos, 0, 1);
-        grilla.add(botonSalir, 0, 2);
+        botonera.add(botonJugar, 0, 0);
+        botonera.add(botonAcercaDe, 0, 1);
+        botonera.add(botonSalir, 0, 2);
 
-        raiz.setTop(titulo);
-        BorderPane.setAlignment(titulo, Pos.CENTER);
-        raiz.setCenter(grilla);
+        raiz.setCenter(botonera);
 
-        stage.setTitle(TITULO_VENTANA);
         Scene escena = new Scene(raiz, ANCHO, ALTO);
         escena.getStylesheets().add(ARCHIVO_ESTILOS);
-        stage.setScene(escena);
-        stage.show();
+
+        escenario.setTitle(TITULO_VENTANA);
+        escenario.resizableProperty().setValue(false);
+        escenario.setScene(escena);
+
+        escenario.show();
     }
 }
