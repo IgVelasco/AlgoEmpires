90b60bb9e523c227339c8b5ff96bcfc079e674b5
iPetrignani <32021777+iPetrignani@users.noreply.github.com>
2018-12-01 17:29:39 -0300

    Add player init scene

--- src/vista/AlgoEmpires.java
+++ src/vista/AlgoEmpires.java
@@ -79,4 +79,62 @@ public class AlgoEmpires extends Application {
         sonidoInicio.play();
         escenario.show();
     }
+
+    void iniciarJugadores() {
+        BorderPane raiz = new BorderPane();
+
+        Scene escena = new Scene(raiz, ANCHO, ALTO);
+        escena.getStylesheets().setAll(ARCHIVO_ESTILOS);
+
+        Button botonAtras = new Button("\uD83E\uDC44 Atrás");
+        botonAtras.setOnAction(new BotonAtrasEventHandler(this.escenario, this.escenaInicial));
+        botonAtras.setAlignment(Pos.CENTER_LEFT);
+
+        raiz.setBottom(botonAtras);
+
+        GridPane formularios = new GridPane();
+        formularios.setAlignment(Pos.CENTER);
+        formularios.setHgap(32);
+        formularios.setVgap(16);
+        setColumnasCentradas(formularios, 2);
+
+        Label jugador1 = new Label("Jugador 1:");
+        jugador1.setId("nombreJugador");
+
+        TextField nombre_jugador1 = new TextField();
+        nombre_jugador1.setId("campoJugador");
+
+        Label jugador2 = new Label("Jugador 2:");
+        jugador2.setId("nombreJugador");
+
+        TextField nombre_jugador2 = new TextField();
+        nombre_jugador2.setId("campoJugador");
+
+        Button botonWololo = new Button("Wololo ⚔");
+        botonWololo.setId("botonWololo");
+        botonWololo.setOnAction(new BotonWololoEventHandler(this, nombre_jugador1, nombre_jugador2));
+
+        nombre_jugador1.setOnAction(actionEvent -> nombre_jugador2.requestFocus());
+
+        formularios.add(jugador1, 0, 0);
+        formularios.add(nombre_jugador1, 0, 1);
+        formularios.add(jugador2, 1, 0);
+        formularios.add(nombre_jugador2, 1, 1);
+        formularios.add(botonWololo, 0, 4, 2, 1);
+
+        raiz.setCenter(formularios);
+
+        raiz.setPadding(new Insets(16));
+        nombre_jugador1.requestFocus();
+        this.escenario.setScene(escena);
+    }
+
+    private void setColumnasCentradas(GridPane grilla, int cantidadColumnas) {
+        ColumnConstraints columnas = new ColumnConstraints();
+        columnas.setHalignment(HPos.CENTER);
+
+        for (int i = 0; i < cantidadColumnas; i++) {
+            grilla.getColumnConstraints().add(columnas);
+        }
+    }
 }
