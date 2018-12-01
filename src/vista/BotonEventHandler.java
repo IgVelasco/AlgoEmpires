ba48020fa04f1a43ebc17be522b1a369c2c93939
iPetrignani <32021777+iPetrignani@users.noreply.github.com>
2018-12-01 17:24:26 -0300

    Refactor generic button event handler

--- src/vista/BotonEventHandler.java
+++ src/vista/BotonEventHandler.java
@@ -2,34 +2,15 @@ package vista;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
 import javafx.scene.media.AudioClip;
-import javafx.stage.Stage;
 
 public class BotonEventHandler implements EventHandler<ActionEvent> {
-    private Button boton;
-    private Stage escenario;
 
-    public BotonEventHandler(Stage escenario, Button boton) {
-        this.boton = boton;
-        this.escenario = escenario;
-    }
-
-    public void BotonEventHandler(Button boton) {
-        this.boton = boton;
-    }
+    private static final AudioClip sonidoClick = new AudioClip(
+            BotonEventHandler.class.getResource("sonidos/click_boton.mp3").toExternalForm()
+    );
 
     public void handle(ActionEvent actionEvent) {
-        Alert wip = new Alert(Alert.AlertType.INFORMATION);
-        wip.setTitle("WIP");
-        wip.setContentText("WIP");
-
-        AudioClip sonidoClick = new AudioClip(
-                getClass().getResource("sonidos/click_boton.mp3").toExternalForm()
-        );
-
         sonidoClick.play();
-        wip.show();
     }
 }
