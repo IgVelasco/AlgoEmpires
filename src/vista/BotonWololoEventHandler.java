11a4dd8f082985f6207edd74a47dd5afa8d35e03
iPetrignani <32021777+iPetrignani@users.noreply.github.com>
2018-12-05 02:55:10 -0300

    Add first map view

--- src/vista/BotonWololoEventHandler.java
+++ src/vista/BotonWololoEventHandler.java
@@ -1,15 +1,24 @@
 package vista;
 
 import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 public class BotonWololoEventHandler extends BotonEventHandler {
-    private final AlgoEmpires aplicacion;
+    private static final int ANCHO = 20;
+    private static final int ALTO = 15;
+    private final Stage escenario;
     private final TextField nombre_jugador1;
     private final TextField nombre_jugador2;
 
-    BotonWololoEventHandler(AlgoEmpires aplicacion, TextField nombre_jugador1, TextField nombre_jugador2) {
-        this.aplicacion = aplicacion;
+    BotonWololoEventHandler(Stage escenario, TextField nombre_jugador1, TextField nombre_jugador2) {
+        this.escenario = escenario;
         this.nombre_jugador1 = nombre_jugador1;
         this.nombre_jugador2 = nombre_jugador2;
     }
@@ -17,7 +26,22 @@ public class BotonWololoEventHandler extends BotonEventHandler {
     @Override
     public void handle(ActionEvent actionEvent) {
         super.handle(actionEvent);
-        System.out.println((nombre_jugador1.getText()));
-        System.out.println((nombre_jugador2.getText()));
+        BorderPane raiz = new BorderPane();
+        GridPane mapa = new GridPane();
+        mapa.setAlignment(Pos.CENTER);
+
+        for (int x = 0; x < ANCHO; x++) {
+            for (int y = 0; y < ALTO; y++) {
+                Image imagenSuelo = new Image(getClass().getResourceAsStream("imagenes/suelo.png"));
+                ImageView suelo = new ImageView(imagenSuelo);
+                mapa.add(suelo, x, y);
+            }
+        }
+
+        raiz.setCenter(mapa);
+        Scene escenaJuego = new Scene(raiz);
+
+        this.escenario.setScene(escenaJuego);
+        this.escenario.setMaximized(true);
     }
 }
