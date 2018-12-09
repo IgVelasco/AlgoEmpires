f378f51268eb194d76a0c8869272929b34b9e535
iPetrignani <32021777+iPetrignani@users.noreply.github.com>
2018-12-09 20:54:02 -0300

    Optimize view imports

--- src/vista/JuegoVista.java
+++ src/vista/JuegoVista.java
@@ -1,15 +1,9 @@
 package vista;
 
 import controlador.*;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ContextMenu;
-import javafx.scene.control.MenuItem;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.ContextMenuEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
