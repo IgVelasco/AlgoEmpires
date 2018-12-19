f85d8a2c73084f754674b4c642be7205be2a29d7
Juan Pablo Donato <juan.pablo.donato@hotmail.com>
2018-12-19 20:11:10 -0300

    CimientoTest completado

--- test/CimientoTest.java
+++ test/CimientoTest.java
@@ -4,15 +4,16 @@ import modelo.estructuras.Cuartel;
 import modelo.excepciones.CasilleroOcupado;
 import modelo.excepciones.CimientoFinalizado;
 import modelo.excepciones.ExcedeLimiteDelMapa;
+import modelo.juego.Juego;
 import modelo.juego.Jugador;
 import modelo.unidades.Aldeano;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class CimientoTest {
 
-  //Al usar la posicion del aldeano y otras cosas del jugador necesito tener todas las clases por eso falla
     @Test
     public void testCimientoGuardaElTipoDeEstructuraConLaCantidadDeTurnosCorrespondiente() {
         Mapa mapa = new Mapa(20,20);
@@ -23,46 +24,92 @@ public class CimientoTest {
         assertEquals(cuartel, cimiento.getFuturaEstructura());
     }
 
-   /*
 
-     @Test
-    public void testCimientoGuardaElTipoDeEstructuraConLaCantidadDeTurnosCorrespondiente() {
-
-        Cuartel unCuartel = new Cuartel(null);
-        Mapa unMapa = new Mapa(20,20);
-        Cimiento cimientoCuartel = new Cimiento(unCuartel,unMapa,5,5,5);
-
-        assertEquals(3, cimientoCuartel.getTurnosRestantes());
-    }
 
    @Test
     public void testCimientoAvanzaLosTurnosCuandoSeLoConstruye() {
-
+        Cuartel cuartel = new Cuartel(null);
         Mapa unMapa = new Mapa(20,20);
-        Cimiento unCimiento = new Cimiento(null,unMapa,5,5,5);
+        Cimiento unCimiento = new Cimiento(cuartel,unMapa,5,5,4);
 
         unCimiento.avanzarConstruccion(null);
 
         assertEquals(2, unCimiento.getTurnosRestantes());
     }
 
+
     @Test
     public void testCimientoLiberaAlAldeanoDeSuConstruccion() {
 
-        Mapa unMapa = new Mapa(20,20);
-        Jugador jugador = new Jugador(unMapa , 5 , 5 , null);
+        Juego unJuego = new Juego(20,20);
+
+        Jugador jugador = unJuego.getJugadorActual();
         Aldeano unAldeano = new Aldeano(jugador);
-        Cuartel unCuartel = new Cuartel(null);
-        Cimiento unCimiento = new Cimiento(unCuartel,unMapa,10,10,2);
+        Mapa mapa = unJuego.getMapa();
+        mapa.colocarUnidadEn(unAldeano,4,4);
+        jugador.agregarAccionable(unAldeano);
 
-        unCimiento.avanzarConstruccion(unAldeano);
-        unCimiento.avanzarConstruccion(unAldeano);
-        unCimiento.avanzarConstruccion(unAldeano);
+        jugador.construirCuartel(unAldeano,5,5);
 
-        unAldeano.realizarAccionCorrespondiente();
+        unJuego.siguienteTurno(); //Jug ok
+        unJuego.siguienteTurno(); //jug not ok
+        unJuego.siguienteTurno(); //Jug ok
+        unJuego.siguienteTurno(); //jug not ok
+        unJuego.siguienteTurno(); //Jug ok
+        unJuego.siguienteTurno(); //jug not ok
 
-        assertEquals(jugador.getOro(), 120);
-    }*/
+        unJuego.siguienteTurno();
+        unJuego.siguienteTurno();
+
+        assertEquals(370, jugador.getOro());
+    }
+
+    @Test
+    public void testCimientoPoneLaEstructuraEnSuLugar(){
+        Juego unJuego = new Juego(20,20);
+
+        Jugador jugador = unJuego.getJugadorActual();
+        Aldeano unAldeano = new Aldeano(jugador);
+        Mapa mapa = unJuego.getMapa();
+        mapa.colocarUnidadEn(unAldeano,4,4);
+        jugador.agregarAccionable(unAldeano);
+
+        jugador.construirCuartel(unAldeano,5,5);
+
+        Cimiento elCimiento = (Cimiento) mapa.getContenido(5,5);
+        Cuartel cuartel = (Cuartel) elCimiento.getFuturaEstructura();
+
+        unJuego.siguienteTurno(); //Jug ok
+        unJuego.siguienteTurno(); //jug not ok
+        unJuego.siguienteTurno(); //Jug ok
+        unJuego.siguienteTurno(); //jug not ok
+        unJuego.siguienteTurno(); //Jug ok
+        unJuego.siguienteTurno(); //jug not ok
+
+        assertEquals(cuartel, mapa.getContenido(5,5));
+
+    }
+
+    @Test
+    public void testCimientoDesapareceCuandoSeLoAtaca(){
+
+        Juego unJuego = new Juego(20,20);
+
+        Jugador jugador = unJuego.getJugadorActual();
+        Aldeano unAldeano = new Aldeano(jugador);
+        Mapa mapa = unJuego.getMapa();
+        mapa.colocarUnidadEn(unAldeano,4,4);
+        jugador.agregarAccionable(unAldeano);
+
+        jugador.construirCuartel(unAldeano,5,5);
+
+        Cimiento elCimiento = (Cimiento) mapa.getContenido(5,5);
+
+        elCimiento.recibirAtaque(1);
+
+        assertNull(mapa.getContenido(5,5));
+
+    }
 
 
 }
