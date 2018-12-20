eb156b3667f576523e0a6e9fb75ee6a3e24d6d0a
iPetrignani <32021777+iPetrignani@users.noreply.github.com>
2018-12-20 02:43:43 -0300

    Add attack alerts

--- src/controlador/AtacarHandler.java
+++ src/controlador/AtacarHandler.java
@@ -5,6 +5,10 @@ import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import modelo.espacio.Contenible;
 import modelo.espacio.Posicion;
+import modelo.excepciones.ContenibleDelMismoJugador;
+import modelo.excepciones.ContenibleFueraDeRango;
+import modelo.excepciones.ContenibleNoPropia;
+import modelo.excepciones.UnidadYaAtaco;
 import modelo.juego.Juego;
 import modelo.unidades.Arquero;
 import modelo.unidades.Atacante;
@@ -30,7 +34,18 @@ public class AtacarHandler extends AccionSobreCasilla implements EventHandler<Ac
     @Override
     public void realizarAccion(MapaView mapaView, Posicion posicion) {
         Contenible unContenible = mapaView.getMapa().getContenido(posicion.getPosX(), posicion.getPosY());
-        atacante.atacar(unContenible, juego.getJugadorActual());
+        try {
+            atacante.atacar(unContenible, juego.getJugadorActual());
+        } catch (ContenibleNoPropia e) {
+            alertar("¡No te pertence!");
+        } catch (UnidadYaAtaco e) {
+            alertar("¡Ya atacó!");
+        } catch (ContenibleFueraDeRango e) {
+            alertar("¡Fuera de rango!");
+        } catch (ContenibleDelMismoJugador e) {
+            alertar("¡Unidad propia!");
+        }
+        
         JuegoVista juegoVista = JuegoVista.getInstancia();
         juegoVista.actualizar(juego);
     }
