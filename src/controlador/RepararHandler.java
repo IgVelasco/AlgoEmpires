3c96cda344ee57948dd844392ec6c9919af5f37b
iPetrignani <32021777+iPetrignani@users.noreply.github.com>
2018-12-20 02:59:43 -0300

    Add repair alerts

--- src/controlador/RepararHandler.java
+++ src/controlador/RepararHandler.java
@@ -5,6 +5,8 @@ import javafx.event.EventHandler;
 import modelo.espacio.Contenible;
 import modelo.espacio.Posicion;
 import modelo.estructuras.Estructura;
+import modelo.excepciones.AldeanoOcupado;
+import modelo.excepciones.EdificioConVidaMaxima;
 import modelo.juego.Juego;
 import modelo.unidades.Aldeano;
 import vista.JuegoVista;
@@ -27,7 +29,14 @@ public class RepararHandler extends AccionSobreCasilla implements EventHandler<A
 
     public void realizarAccion(MapaView mapaView, Posicion posicion) {
         Contenible unContenible = mapaView.getMapa().getContenido(posicion.getPosX(), posicion.getPosY());
-        juego.getJugadorActual().repararEstructura(aldeano, (Estructura) unContenible);
+        try {
+            juego.getJugadorActual().repararEstructura(aldeano, (Estructura) unContenible);
+        } catch (AldeanoOcupado e) {
+            alertar("¡Aldeano ocupado!");
+        } catch (EdificioConVidaMaxima e) {
+            alertar("¡Vida máxima alcanzada!");
+        }
+
         JuegoVista juegoVista = JuegoVista.getInstancia();
         juegoVista.actualizar(mapaView.getJuego());
     }
